cf8306502edd36952570f129792fa1de5bb8acef
Nikita Kovalyov <boba.bibap@gmail.com>
2022-05-24 01:05:48 +0300

    project-complete

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+    return <footer className="page-footer grey darken-1 ">
+    <div className="footer-copyright">
+      <div className="container">
+      © 2022 Copyright Text
+      <a className="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
+}
